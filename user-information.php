1e029306a22bafc69189ab06b060d9d1d16144d1
sanjay yadav <sanjaydotlive@gmail.com>
2019-01-19 13:49:04 +0530

    updated

--- user-information.php
+++ user-information.php
@@ -12,14 +12,23 @@ curl_setopt_array($curl, array(
   CURLOPT_TIMEOUT => 30,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => "GET",
-  CURLOPT_POSTFIELDS => "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"mobile_number\"\r\n\r\n8802034056\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--",
   CURLOPT_HTTPHEADER => array(
     "Accept: application/json",
     "Authorization: Bearer $personal_access_token",
-    "cache-control: no-cache",
-    "content-type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW"
+    "cache-control: no-cache"
   ),
 ));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
 
 $response = curl_exec($curl);
 $err = curl_error($curl);
